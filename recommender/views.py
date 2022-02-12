864106c0114e16a11d7626be6225b9be85e4d28e
Emily O'Connell <emilykoconnell@gmail.com>
2022-02-11 19:51:24 -0600

    recommender views

--- recommender/views.py
+++ recommender/views.py
@@ -1,21 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-import requests
-from .models import Greeting
 
 # Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+from django.http import HttpResponse
+from django.contrib import messages
 
+'''from .models import xyz'''
+# request handler!
 
-def db(request):
+# create view ==> add to urls.py here, then in parent class.
+def rec_home(request):
 
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
-
+    return render(request, 'rec_home.html')# , context=context)
