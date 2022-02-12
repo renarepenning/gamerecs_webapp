0c81a91bc38636bda54db4f09ef0f6041a254af4
Emily O'Connell <emilykoconnell@gmail.com>
2022-02-11 19:52:39 -0600

    change home to index.html

--- gamerecs/views.py
+++ gamerecs/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # create view ==> add to urls.py here, then in parent class.
 def home(request):
-    return render(request, 'home.html') #, context=context)
+    return render(request, 'index.html') #, context=context)
 
     #return HttpResponse('HOME PAGE \n Welcome to our capstone website')
 
