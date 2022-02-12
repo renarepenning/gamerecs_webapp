0bc1cc0a545a975604607e3d2241d82b1abd1538
Emily O'Connell <emilykoconnell@gmail.com>
2022-02-11 19:07:08 -0600

    allowed host

--- gamerecs/settings.py
+++ gamerecs/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [game-recs.herokuapp.com]
+ALLOWED_HOSTS = ['game-recs.herokuapp.com']
 
 
 # Application definition
