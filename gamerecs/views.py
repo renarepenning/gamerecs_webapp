82e7de8493f433b0c2e8d3e8fb95edcc26b8e3a6
Emily O'Connell <emilykoconnell@gmail.com>
2022-02-11 19:04:34 -0600

    import django_heroku

--- gamerecs/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# create view ==> add to urls.py here, then in parent class.
-def home(request):
-    return render(request, 'home.html') #, context=context)
-
-    #return HttpResponse('HOME PAGE \n Welcome to our capstone website')
--- recommender/views.py
+++ recommender/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import requests
+from .models import Greeting
 
 # Create your views here.
-from django.http import HttpResponse
-from django.contrib import messages
+def index(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, "index.html")
 
+
+<<<<<<< HEAD
 '''from .models import xyz'''
 # request handler!
 
 # create view ==> add to urls.py here, then in parent class.
 def rec_home(request):
+=======
+def db(request):
 
-    return render(request, 'rec_home.html')# , context=context)
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+>>>>>>> change-name
+
+    return render(request, "db.html", {"greetings": greetings})
