1b5ada6164f4688f81f6d61be35690b9184de479
Rena Repenning <renareps108@gmail.com>
2022-02-16 13:00:48 -0600

    add model

--- recommender/models.py
+++ recommender/models.py
@@ -3,9 +3,12 @@ from django.db import models
 
 # Create your models here.
 
+# creating dj model: resiger in settings, create in models, add in admin
+
 
 class User(models.Model):
     userName = models.CharField(max_length=20)
     age = models.CharField(max_length=3)
     genre = models.CharField(max_length=10)
     keyword = models.CharField(max_length=10)
+    theme = models.CharField(max_length=10)
