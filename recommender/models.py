ce687a86289f9b9fd8bed981b81283611525cc48
Rena Repenning <renareps108@gmail.com>
2022-02-16 12:45:21 -0600

    add model

--- recommender/models.py
+++ recommender/models.py
@@ -1,3 +1,11 @@
+from ast import keyword
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    userName = models.CharField(max_length=20)
+    age = models.CharField(max_length=3)
+    genre = models.CharField(max_length=10)
+    keyword = models.CharField(max_length=10)
