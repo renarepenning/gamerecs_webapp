1410f7f587574dfcc8b1c21141602cf3e25b818a
Rena Repenning <renareps108@gmail.com>
2022-03-09 15:50:27 -0600

    rm postgres

--- gamerecs/settings.py
+++ gamerecs/settings.py
@@ -16,8 +16,8 @@ import psycopg2
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-"""BASE_DIR = Path(__file__).resolve().parent.parent
-"""
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -25,6 +25,7 @@ import psycopg2
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-qyue=0afswnau(#_rg(i96=*8cw$jgw=&(m5ut^n18h3f)y2-a'
 # SECRET_KEY = os.environ.get("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -86,12 +87,14 @@ WSGI_APPLICATION = 'gamerecs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd8sbqfan0c1jo0',
-        'HOST': 'ec2-50-19-32-96.compute-1.amazonaws.com',
-        'PORT' : 5432,
-        'USER' : 'ivdeaeepvsgroc',
-        'PASSWORD' : '3d09e9aec99ecce34cd6534d65f5beda3090214f081be26e40fed8e25f1b0d7e'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'd8sbqfan0c1jo0',
+        # 'HOST': 'ec2-50-19-32-96.compute-1.amazonaws.com',
+        # 'PORT' : 5432,
+        # 'USER' : 'ivdeaeepvsgroc',
+        # 'PASSWORD' : '3d09e9aec99ecce34cd6534d65f5beda3090214f081be26e40fed8e25f1b0d7e'
     }
 }
 
