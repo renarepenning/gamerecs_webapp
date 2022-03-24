bca13b09452b98172529e13406610d1f59dbb31e
Rena Repenning <renareps108@gmail.com>
2022-03-23 21:37:54 -0500

    ugh

--- gamerecs/settings.py
+++ gamerecs/settings.py
@@ -139,10 +139,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR / 'static']
+STATICFILES_DIRS =[ BASE_DIR / 'static']
 
+ 
+STATIC_ROOT = os.path.join(BASE_DIR, 'gamerecs')
 """
-#STATIC_ROOT = os.path.join(BASE_DIR, 'gamerecs'),
 STATICFILES_DIRS = [
     #os.path.join(BASE_DIR, 'static'),
     #os.path.join(BASE_DIR, 'boot'),
