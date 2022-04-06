833d4a23a4369f899d88b8ff1b7763f89f54e350
Rena Repenning <renareps108@gmail.com>
2022-04-06 15:13:21 -0500

    minor styling

--- recommender/views.py
+++ recommender/views.py
@@ -1,3 +1,4 @@
+from sqlite3 import Timestamp
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
@@ -32,7 +33,7 @@ def user_view(request):
     # https://www.youtube.com/watch?v=VxOsCKMStuw
     userid = request.user.pk # gives primary key
     entries = Entry.objects.all().filter(user_id=userid)
-    inputs = Rec.objects.all().filter(user_id=userid)
+    inputs = Rec.objects.all().filter(user_id=userid).order_by('timestamp')[:10:-1]
     args = {'user': request.user, 'entries': entries, 'recs': inputs}
     return render(request, "user.html", args)
 
@@ -58,11 +59,7 @@ def rate(request):
         val = request.POST.get('val')
         obj = Rec.objects.get(id=el_id)
         obj.rating = int(val)
-
-
         obj.save()
-        # print("rec saved correctly! ==> ", obj, " ", obj.rating)
-
 
         # https://stackoverflow.com/questions/50782502/django-save-method-not-saving
         return JsonResponse({'success':'true', 'rating': obj.rating}, safe=False)
